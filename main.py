65e88001064eb9ec64fa4fc5b17b28df63081832
Bao Vuong <vqbao@lv5.org>
2025-08-06 00:39:47 -0400

    logging

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import json
+import logging 
+import os 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import tomllib
+
+scope = 'playlist-read-private,user-library-read'
+
+with open(".env.toml", mode="rb") as fp:
+    config = tomllib.load(fp)
+
+logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+rootLogger = logging.getLogger()
+rootLogger.setLevel(logging.INFO)
+
+fileHandler = logging.FileHandler("{0}/{1}.log".format(config['log']['path'], config['log']['filename']))
+fileHandler.setLevel(logging.INFO)
+fileHandler.setFormatter(logFormatter)
+rootLogger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setLevel(logging.INFO)
+consoleHandler.setFormatter(logFormatter)
+rootLogger.addHandler(consoleHandler)
 
 # authenticate spotify account
 def authenticate(username, password):
@@ -15,32 +36,36 @@ def get_all_saved_tracks(sp: spotipy.Spotify, limit=50):
     tracks = []
     offset = 0
 
+    rootLogger.debug('grabbing tracks: limit={0} offset={1}', limit, offset)
     current = sp.current_user_saved_tracks(limit, offset)['items']
     while current:
         tracks.extend(current)
         offset += limit
+        rootLogger.debug('grabbing tracks: limit={0} offset={1}', limit, offset)
         current = sp.current_user_saved_tracks(limit, offset)['items']
 
     return tracks
 def main():
-    with open("app.toml", mode="rb") as fp:
-        config = tomllib.load(fp)
-
-    scope = 'playlist-read-private,user-library-read'
-
+    rootLogger.info('starting Spotify Extraction')
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-        client_id=config['spotify_app']['client_id'],
-        client_secret=config['spotify_app']['client_secret'],
-        redirect_uri=config['spotify_app']['redirect_uri'],
+        client_id=config['api']['client_id'],
+        client_secret=config['api']['client_secret'],
+        redirect_uri=config['api']['redirect_uri'],
         scope=scope))
 
+    # setting up files
+    if not os.path.isdir('output'):
+        os.makedirs('output')
+    
+
+    rootLogger.info('Getting all saved tracks')
     saved_tracks = get_all_saved_tracks(sp)
-    print(len(saved_tracks))
 
+    rootLogger.info('saving tracks to file')
+    with open('output/saved_tracks.json', 'w') as f:
+        json.dump(saved_tracks, f, indent=4)
 
-    with open('saved_tracks.json', 'w') as f:
-        json.dump(saved_tracks, f)
-
+    rootLogger.info('Spotify Extraction complete')
 
 if __name__ == '__main__':
     main()
