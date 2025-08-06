285bbc25b5d1114005bd73fd0bd2aa304ba0313f
Bao Vuong <vqbao@lv5.org>
2025-08-06 18:58:08 -0400

    model stubs

---
+++ models.py
@@ -0,0 +1,27 @@
+class Track:
+
+    def __init__(self, id='', name='', artist=None, album=None):
+        self.id = id
+        self.name = name
+        self.artist = artist 
+        self.album = album
+
+class Artist:
+    def __init__(self, id='', name=''):
+        self.id = id
+        self.name = name  
+
+class Album:
+    
+    def __init__(self, id='', name='', tracks=[], year=0):
+        self.id = id
+        self.name = name 
+        self.tracks = tracks
+        self.year = year 
+
+class Playlist:
+    
+    def __init__(self, id='', name='', tracks=[]):
+        self.id = id 
+        self.name = name
+        self.tracks = tracks
